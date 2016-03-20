1a129e324b6de9b16eeed62d467fd4be83f8fa15
He Shengyi <iamheshengyi@126.com>
2016-03-20 10:46:34 +0800

    bug fix

--- src/netqu/somefinals.java
+++ src/netqu/somefinals.java
@@ -1,9 +1,9 @@
 package netqu;
 
 public  class somefinals {
-	public final static int ROWS=5;
-	public final static int COLS=5;
-	public final static int DIANSHU=ROWS*COLS;
+//	public final static int ROWS=5;
+//	public final static int COLS=5;
+	public final static int DIANSHU=30;
 //	public final static int x0=100;
 //	public final static int y0=230;
 	public final static int x0=100;
@@ -11,7 +11,11 @@ public  class somefinals {
 	public final static int r=210;
 //	public final static int edgesnum=10;
 	public final static int INF=1<<20;
-	public final static int   RowInter=(int)(400/ROWS);
-	public final static int  ColInter=(int)(400/COLS);
+	public final static int WIDTH=300;
+	public final static int HEIGHT=300;
+//	public final static double DISPOWER=150*150;
+	public final static double COVERDIS=70;
+//	public final static int   RowInter=(int)(400/ROWS);
+//	public final static int  ColInter=(int)(400/COLS);
 	
 }
