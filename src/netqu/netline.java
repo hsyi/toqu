ce1d522b367f7dfd6cfb27712979c829b8a93e4d
He Shengyi <iamheshengyi@126.com>
2016-03-20 10:29:56 +0800

    rangecover

---
+++ src/netqu/netline.java
@@ -0,0 +1,10 @@
+package netqu;
+
+public class netline {
+	public int p1;
+	public int p2;
+	public double bw_h,bw_l,jt_h,jt_l,er_h,er_l,dl_h,dl_l,en_h,en_l,tr_h,tr_l;
+	public netline(){
+		
+	}
+}
---
+++ src/netqu/somefinals.java
@@ -0,0 +1,17 @@
+package netqu;
+
+public  class somefinals {
+	public final static int ROWS=5;
+	public final static int COLS=5;
+	public final static int DIANSHU=ROWS*COLS;
+//	public final static int x0=100;
+//	public final static int y0=230;
+	public final static int x0=100;
+	public final static int y0=100;
+	public final static int r=210;
+//	public final static int edgesnum=10;
+	public final static int INF=1<<20;
+	public final static int   RowInter=(int)(400/ROWS);
+	public final static int  ColInter=(int)(400/COLS);
+	
+}
